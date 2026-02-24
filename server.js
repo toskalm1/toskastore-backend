fc719c5d5553fd58566b11c1aa24fa3a24daaec3
toskalm1 <ruhzero1984@gmail.com>
2026-02-24 14:28:49 +0300

    Connect to MongoDB on server startup

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Toskastore Backend Running 🚀");
 });
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected 🚀"))
+.catch(err => console.log("MongoDB Error:", err));
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started");
 });
